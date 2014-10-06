d314fe57701a8c627c45c9ac99bddcb59d0398b7
cristian Angel arrieta sagun <sagun@iota21.aulas.gsyc.es>
2014-10-06 12:34:25 +0200

    programa empezado

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -20,16 +20,16 @@ class SmallSMILHandler(ContenHandler):
         self.rg-bottom = ""
         self.rg-left = ""
         self.rg-right = ""
-        self.img = ""
+
         self.im-src = ""
         self.im-region = ""
         self.im-begin = ""
         self.im-dur = ""
-        self.audio = ""
+
         self.aud-src = ""
         self.aud-begin = ""
         self.aud-dur = "" 
-        self.textstream = ""
+
         self.ts-src = ""
         self.ts-region = ""
 
@@ -43,10 +43,22 @@ class SmallSMILHandler(ContenHandler):
             self.lo-height = attrs.get('height', "")
             self.lo-background-color = attrs.get('background-color', "")
         elif name == 'region':
-            
+            self.rg.id = attrs.get('id', "")
+            self.rg-top = attrs.get('top', "")
+            self.rg-bottom = attrs.get('bottom', "")
+            self.rg-left = attrs.get('left', "")
+            self.rg-right = attrs.get('right', "")
         elif name == 'img':
-            self.inRespuesta = 1
+            self.im-src = attrs.get('src', "")
+            self.im-region = attrs.get('region', "")
+            self.im-begin = attrs.get('begin', "")
+            self.im-dur = attrs.get('dur', "")
         elif name == 'audio':
+            self.aud-src = attrs.get('src', "")
+            self.aud-begin = attrs.get('begin', "")
+            self.aud-dur = attrs.get('dur', "")
         elif name == 'texstream':
+            self.ts-src = attrs.get('src', "")
+            self.ts-region = attrs.get('region', "")
 
 
