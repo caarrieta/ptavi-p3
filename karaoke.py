8b53c3db1551d79ed7d4a865aad4c13eec35d610
cristian Angel arrieta sagun <sagun@iota18.aulas.gsyc.es>
2014-10-13 13:04:02 +0200

    cambiado

--- karaoke.py
+++ karaoke.py
@@ -4,7 +4,22 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import smallsmilhandler
+import sys
 
-class Karaoke():
+class karaokelocal():
 
+    def __init__(self, fichero):
+       parser = make_parser()
+       sHandler = smallsmilhandler.SmallSMILHandler()
+       parser.setContentHandler(sHandler)
+       parser.parse(open(karaoke.py))
+       self.lista_dic = sHandler.get_tags()
+
+if __name__ == "__main__":
+
+try:
+    fichero = sys.argv[1]
+except IndexError:
+    print "Usage: python karaoke.py file.smil."
+raise SystemExit
 
