fcb85f7b426c971d95c1593980ba01b50b01c5a1
cristian Angel arrieta sagun <sagun@iota18.aulas.gsyc.es>
2014-10-13 13:41:41 +0200

    cambiado

--- karaoke.py
+++ karaoke.py
@@ -6,7 +6,12 @@ from xml.sax.handler import ContentHandler
 import smallsmilhandler
 import sys
 
-
+def imprimirtags(sHandler):
+    for etiqueta in sHandler.lista:
+        print etiqueta[0], atributos=etiqueta[1]
+        for atributo in atributos:
+            print "\t", atributo, "=", atributos[atributo],
+        print
 try:
     parser = make_parser()
     SHandler = smallsmilhandler.SmallSMILHandler()
