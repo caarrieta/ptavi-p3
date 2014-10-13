b68318439ecf377d6b36977769463a4f4cefe55f
cristian Angel arrieta sagun <sagun@iota18.aulas.gsyc.es>
2014-10-13 13:59:41 +0200

    cambiado

--- karaoke.py
+++ karaoke.py
@@ -7,16 +7,17 @@ import smallsmilhandler
 import sys
 
 def imprimirtags(sHandler):
-    for etiqueta in sHandler.lista:
-        print etiqueta[0], atributos=etiqueta[1]
+    for etiqueta in sHandler.tags:
+        print etiqueta[0], 
+        atributos = etiqueta[1]
         for atributo in atributos:
             print "\t", atributo, "=", atributos[atributo],
         print
 try:
     parser = make_parser()
-    SHandler = smallsmilhandler.SmallSMILHandler()
+    sHandler = smallsmilhandler.SmallSMILHandler()
     parser.setContentHandler(sHandler)
-    parser.parse(open(sys.arg[1]))
+    parser.parse(open(sys.argv[1]))
     imprimirtags(sHandler)
 except IndexError:
     print "Usage: python karaoke.py file.smil."
