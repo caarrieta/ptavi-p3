d8a06fe4bfbd32ecc09b5ff897f3b0a9425c7be5
cristian Angel arrieta sagun <sagun@iota22.aulas.gsyc.es>
2014-10-13 10:28:12 +0200

    guardado

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -12,25 +12,44 @@ class SmallSMILHandler(ContenHandler):
         self.root_lo = []
         self.rg = []
         self.im = []
-	self.aud-src = []
-        self.aud-begin = []
-        self.ts = [] 
+        self.aud = []
+        self.ts = []
 
-
-    def get_tags(self, name, attrs):
+    def StartElement(self, name, attrs):
 
         if name == 'root-layout':
             # De esta manera tomamos los valores de los atributos
             self.root_layout = {}
             self.atributos = (attrs, self.root_layout)
-            self.root_lo.append([name, 
+            self.root_lo.append([name, self.root_layout])
         elif name == 'region':
-      
+            self.region = {}
+            self.atributos = (attrs, self.region)
+            self.rg.append = ([name, self.region])
         elif name == 'img':
-
+            self.img = {}
+            self.atributos = (attrs, self.img)
+            self.im.append = ([name, self.img])
         elif name == 'audio':
-
+            self.audio = {}
+            self.atributos = (attrs, self.audio)
+            self.rg.aud = ([name, self.audio])
         elif name == 'texstream':
+            self.textream = {}
+            self.atributos = (attrs, self.region)
+            self.ts.append = ([name, self.region])
 
-
-
+    def atributos(self, attrs, dic)
+        atrib = attrs.keys()
+        n = 0
+        while n < len(atrib):
+            dic[str(atrib)]
+            n = n + 1
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    parser = make_parser()
+    sHandler = SmallSMILHandler()
+    parser.setContentHandler(sHandler)
+    parser.parse(open('karaoke.xml'))
