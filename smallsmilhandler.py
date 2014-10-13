f8cb3524493669cec167ee243e9fc98b2a85e991
cristian Angel arrieta sagun <sagun@iota18.aulas.gsyc.es>
2014-10-13 14:39:21 +0200

    programa cambiado

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,53 +1,36 @@
 #!/usr/bin/python
-# -*- coding: iso-8859-15 -*-
+# -*- coding: utf-8 -*-
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-
 class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
-
-        self.tags = []
-
-    def StartElement(self, name, attrs):
-
-        if name == 'root-layout':
-            # De esta manera tomamos los valores de los atributos
-            self.root_layout = {}
-            self.atributos = (attrs, self.root_layout)
-            self.tags.append([name, self.root_layout])
-        elif name == 'region':
-            self.region = {}
-            self.atributos = (attrs, self.region)
-            self.tags.append = ([name, self.region])
-        elif name == 'img':
-            self.img = {}
-            self.atributos = (attrs, self.img)
-            self.tags.append = ([name, self.img])
-        elif name == 'audio':
-            self.audio = {}
-            self.atributos = (attrs, self.audio)
-            self.tags.append = ([name, self.audio])
-        elif name == 'texstream':
-            self.textream = {}
-            self.atributos = (attrs, self.region)
-            self.tags.append = ([name, self.region])
-
-    def atributos(self, attrs, dic):
-        atrib = attrs.keys()
-        n = 0
-        while n < len(atrib):
-            dic[str(atrib)]
-            n = n + 1
-
+        self.lista = []
+        self.tags = [
+            'root-layout', 'region', 'img', 'audio', 'textstream']
+        self.atrib = {
+            'root-layout': ['width', 'height'],
+            'region': ['id', 'top', 'left'],
+            'img': ['src', 'region', 'begin', 'dur'],
+            'audio': ['src', 'begin', 'dur'],
+            'textstream': ['src', 'region']
+            }
+    def startElement(self, name, attrs):
+        dic = {}
+        if name in self.tags:
+            dic["name"] = name
+            for key in self.atrib[name]:
+                    dic[key] = attrs.get(key, "")
+            self.lista.append(dic)
     def get_tags(self):
-        return self.tags
+        return self.lista
 
 if __name__ == "__main__":
 
     parser = make_parser()
-    sHandler = SmallSMILHandler()
-    parser.setContentHandler(sHandler)
+    smallhandler = SmallSMILHandler()
+    parser.setContentHandler(smallhandler)
     parser.parse(open('karaoke.smil'))
+    print smallhandler.get_tags()
